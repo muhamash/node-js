072d413956b0d447d6281ab1c839782d5fbd6e6f
muhamsh <muhammad-ashraful@outlook.com>
2025-04-06 15:04:18 +0600

    pipe streaming

---
+++ stream/readStream.js
@@ -0,0 +1,14 @@
+const http = require( 'http' );
+const fs = require('fs');
+
+const pipeServer = http.createServer( ( req, res ) =>
+{
+    const myReadStream = fs.createReadStream( __dirname + '/data.txt', 'utf8' );
+
+    myReadStream.pipe(res)
+} );
+
+
+pipeServer.listen(3000);
+
+console.log(`localhost on!!`)
