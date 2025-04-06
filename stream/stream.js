e09dd6d426326d60d59de78a75d0e09ca8abdcaf
muhamsh <muhammad-ashraful@outlook.com>
2025-04-06 14:57:33 +0600

    node pipe

--- stream/stream.js
+++ stream/stream.js
@@ -1,11 +1,13 @@
 const fs = require( 'fs' );
 
 const myStream = fs.createReadStream( `${__dirname}/data.txt` );
-const myWrite = fs.createWriteStream( `${__dirname}/output.txt` );
+const myWrite = fs.createWriteStream( `${__dirname}/outputPipe.txt` );
 
-myStream.on( 'data', ( chunk ) =>
-{
-    // console.log( chunk );
-    // console.log(chunk.toString());
-    myWrite.write( chunk );
-} );
+// myStream.on( 'data', ( chunk ) =>
+// {
+//     // console.log( chunk );
+//     // console.log(chunk.toString());
+//     myWrite.write( chunk );
+// } );
+
+myStream.pipe( myWrite );
