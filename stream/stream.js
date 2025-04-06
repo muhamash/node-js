be23ba634337022cfbb1ad1a0d48ed6fdedc6b3c
muhamsh <muhammad-ashraful@outlook.com>
2025-04-06 14:55:10 +0600

    stream read to write

--- stream/stream.js
+++ stream/stream.js
@@ -1,9 +1,11 @@
 const fs = require( 'fs' );
 
 const myStream = fs.createReadStream( `${__dirname}/data.txt` );
+const myWrite = fs.createWriteStream( `${__dirname}/output.txt` );
 
 myStream.on( 'data', ( chunk ) =>
 {
     // console.log( chunk );
     // console.log(chunk.toString());
+    myWrite.write( chunk );
 } );
